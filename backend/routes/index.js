b273f4fd4a121b545650965eb965609089e55274
Sury12213 <lyminhhai102@gmail.com>
2025-04-19 12:57:07 +0700

    CRUD khuyenmai

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const movieRoutes = require('./movieRoutes');
 const theaterRoutes = require('./theaterRoutes');
+const promotionRoutes = require('./promotionRoutes');
 // const bookingRoutes = require('./bookingRoutes');
 // const userRoutes = require('./userRoutes');
 
 router.use('/movies', movieRoutes);
 router.use('/theaters', theaterRoutes);
+router.use('/promotions', promotionRoutes);
 // router.use('/bookings', bookingRoutes);
 // router.use('/users', userRoutes);
 

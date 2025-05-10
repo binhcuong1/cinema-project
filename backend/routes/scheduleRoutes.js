4546394fdc182334af5ffe44e61f3b45094b382b
binhcuong <binhcuongpy2014@gmail.com>
2025-05-10 18:47:09 +0700

    Fix lỗi không hiển thị được âm thanh

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const scheduleController = require('../controllers/scheduleController');
 
-router.get('/:id', scheduleController.getById);
 router.get('/room/:id', scheduleController.getByRoom);
 router.get('/audios/', scheduleController.getAudios);
 router.get('/movie/:movieId/dates', scheduleController.getDatesByTheaterAndMovie);
 router.get('/movie/:movieId/:date/showtimes', scheduleController.getShowtimesByMovieAndDate);
 router.post('/audios/', scheduleController.createAudio);
 router.post('/', scheduleController.createSchedules);
+
+router.get('/:id', scheduleController.getById);
 router.delete('/:id', scheduleController.deleteSchedule);
 
 module.exports = router;
